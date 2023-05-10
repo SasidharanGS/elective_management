aa1c7fc37327f27ef78fd798d5c818752fbff868
sasidharan_gs <sasidharangs.701@gmail.com>
2023-05-10 21:02:49 +0530

    concise routes

--- app/routes/users.js
+++ app/routes/users.js
@@ -9,8 +9,35 @@ router.get("/", (req, res) =>{
     res.send("User List")
 })
 
-router.get("/new", (req, res) =>{
-    res.send("New form!")
+// router.get("/new", (req, res) =>{
+//     res.send("New form!")
+// })
+
+// router.get("/:id", (req, res) =>{
+//     res.send(`Get user with ID ${req.params.id}!`)
+// })
+
+// router.put("/:id", (req, res) =>{
+//     res.send(`Update user with ID ${req.params.id}!`)
+// })
+
+// router.delete("/:id", (req, res) =>{
+//     res.send(`Delete user with ID ${req.params.id}!`)
+// })
+
+//A MORE CONCISE WAY OF DOING THE ABOVE IS:
+router.route("/:id")
+.get((req, res) =>{
+    res.send(`Get user with ID ${req.params.id}!`)
 })
+.put((req, res) =>{
+    res.send(`Update user with ID ${req.params.id}!`)
+})
+.delete((req, res) =>{
+    res.send(`Delete user with ID ${req.params.id}!`)
+})
+
+//Remember that the routing goes top to bottom on this page
+//So put the strongest condition last!
 
 module.exports = router
